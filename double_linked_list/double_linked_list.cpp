e23d286cd2b462e8631fb6b72a1b485f0d0dba13
finanovita <finanovita479@gmail.com>
2024-05-02 11:08:54 +0700

    Add project files.

---
+++ double_linked_list/double_linked_list.cpp
@@ -0,0 +1,21 @@
+// double_linked_list.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    int noMhs;
+    string name;
+    Node *next;
+    Node* prev;
+};
+
+
+int main()
+{
+    
+}
+
