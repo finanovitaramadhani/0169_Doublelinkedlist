1d8d0a037a4c8beb08d8855b134d390086cd779d
finanovita <finanovita479@gmail.com>
2024-05-04 12:30:41 +0700

    Membuat prosedur add node

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -13,6 +13,17 @@ struct Node
     Node* prev;
 };
 
+Node* START = NULL;
+
+void addNode() {
+    Node* newNode = new Node();     // step 1: create anew node
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;          // Assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;           // Assign value to the data field of the new node
+
+}
+
 
 int main()
 {
